eb540a0fe1298cc2b0522d64542b8126c564692e
Fabio Vettori <fabio_vettori@hotmail.it>
2020-12-16 18:33:25 +0100

    init

---
+++ index.php
@@ -0,0 +1,24 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+    <head>
+        <meta charset="utf-8">
+        <title>FAQ - Privacy and Terms - Google</title>
+        <link rel="stylesheet" href="public\style.css">
+    </head>
+    <body>
+
+        <header>
+
+        </header>
+
+        <main>
+            
+        </main>
+
+        <footer>
+
+        </footer>
+
+    </body>
+</html>
